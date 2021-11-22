5aed78d0a33aff014ee0083c79659ddf24c859f8
jayantchaudhary002 <56104687+jayantchaudhary002@users.noreply.github.com>
2021-11-22 23:54:12 +0530

    Deleting old data

--- prisma-backend/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URL: process.env.MONGO_URI,
-    JWT_SECRET: process.env.JWT_SECRET || "test",
-    JWT_EXPIRY: process.env.JWT_EXPIRY || "7d"
-};
